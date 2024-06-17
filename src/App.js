57b3a79c5ff90f56f19e3658f01467dffeccd031
Ozysy <artgagarin2@gmail.com>
2024-06-17 09:29:10 +0400

    Fix routing and deployment settings

--- src/App.js
+++ src/App.js
@@ -1,56 +1,35 @@
-import React from 'react'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import React from 'react';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 
-
-/** import all components */
-import Username from './components/Username';
+// import all components
+import UserName from './components/UserName';
 import Password from './components/Password';
 import Register from './components/Register';
 import Profile from './components/Profile';
 import Recovery from './components/Recovery';
 import Reset from './components/Reset';
 import PageNotFound from './components/PageNotFound';
+import Main from './components/Main';
 
+// auth middleware
+import { AuthorizeUser, ProtectRoute } from './middleware/auth';
 
-/** auth middleware */
-import { AuthorizeUser, ProtectRoute } from './middleware/auth'
-
-/** root routes */
-const router = createHashRouter([
-    {
-        path : '/',
-        element : <Username></Username>
-    },
-    {
-        path : '/register',
-        element : <Register></Register>
-    },
-    {
-        path : '/password',
-        element : <ProtectRoute><Password /></ProtectRoute>
-    },
-    {
-        path : '/profile',
-        element : <AuthorizeUser><Profile /></AuthorizeUser>
-    },
-    {
-        path : '/recovery',
-        element : <Recovery></Recovery>
-    },
-    {
-        path : '/reset',
-        element : <Reset></Reset>
-    },
-    {
-        path : '*',
-        element : <PageNotFound></PageNotFound>
-    },
-])
-
+// root routes
 export default function App() {
   return (
     <main>
-        <RouterProvider router={router}></RouterProvider>
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/username" element={<UserName />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/password" element={<ProtectRoute><Password /></ProtectRoute>} />
+          <Route path="/profile" element={<AuthorizeUser><Profile /></AuthorizeUser>} />
+          <Route path="/recovery" element={<Recovery />} />
+          <Route path="/reset" element={<Reset />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </HashRouter>
     </main>
-  )
+  );
 }
