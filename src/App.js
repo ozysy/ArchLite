a3a0aee535c893a19e51036f15ec235b7f4735d7
Ozysy <artgagarin2@gmail.com>
2024-06-17 09:21:32 +0400

    Fix routing and deployment settings

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-// import all components
+// импорт всех компонентов
 import UserName from './components/UserName';
 import Password from './components/Password';
 import Register from './components/Register';
@@ -11,54 +11,57 @@ import Reset from './components/Reset';
 import PageNotFound from './components/PageNotFound';
 import Main from './components/Main';
 
-
-
-//auth middleware
-import { AuthorizeUser } from './middleware/auth';
-import { ProtectRoute } from './middleware/auth';
+// auth middleware
+import { AuthorizeUser, ProtectRoute } from './middleware/auth';
 
 // root routes
 const router = createBrowserRouter([
-    {
-        path : '/',
-        element : <Main></Main>
-    },
-    {
-        path : '/Username',
-        element : <UserName></UserName>
-    },
-    {
-        path : '/register',
-        element : <Register></Register>
-    },
-    {
-        path : '/password',
-        element :<ProtectRoute><Password /></ProtectRoute>
-    },
-    {
-        path : '/profile',
-        element :<AuthorizeUser><Profile /></AuthorizeUser>
-    },
-    {
-        path : '/recovery',
-        element : <Recovery></Recovery>
-    },
-    {
-        path : '/reset',
-        element : <Reset></Reset>
-    },
-    {
-        path : '/*',
-        element : <PageNotFound></PageNotFound>
-    },
-    
-])
-
+  {
+    path: '/',
+    element: <Main />
+  },
+  {
+    path: '/username',
+    element: <UserName />
+  },
+  {
+    path: '/register',
+    element: <Register />
+  },
+  {
+    path: '/password',
+    element: (
+      <ProtectRoute>
+        <Password />
+      </ProtectRoute>
+    )
+  },
+  {
+    path: '/profile',
+    element: (
+      <AuthorizeUser>
+        <Profile />
+      </AuthorizeUser>
+    )
+  },
+  {
+    path: '/recovery',
+    element: <Recovery />
+  },
+  {
+    path: '/reset',
+    element: <Reset />
+  },
+  {
+    path: '/*',
+    element: <PageNotFound />
+  }
+]);
 
 export default function App() {
-    return (
-        <main>
-        <RouterProvider router={router}></RouterProvider>
+  return (
+    <main>
+      <RouterProvider router={router} basename="/ArchLite" />
     </main>
-    )
+  );
 }
